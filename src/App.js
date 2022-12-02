885dc3c30e1225e86e81a3e13371f4cbf2573ba8
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-12-02 14:48:17 -0800

    quickfix 4 frontend

--- src/App.js
+++ src/App.js
@@ -15,100 +15,86 @@ let SERVER = process.env.REACT_APP_SERVER;
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-
-  }
-  render() {
-    return (
-      <>
-      <Header id="header" />
-        
-          {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
-          {this.props.auth0.isAuthenticated ? <Profile /> : <h3>Please Log In</h3>}
-      
-        Hello World
-
-      </>
-    );
-
-    this.state={
-      isBuyOrSellModalShown:false,
-      isPortfolioModalShown:false,
-      userData:{},
+    this.state = {
+      isBuyOrSellModalShown: false,
+      isPortfolioModalShown: false,
+      userData: {},
       userDataIsAvailable: false
     }
   }
 
   // constructor(chartObject, formattedTime) {
-    //   this.ticker = chartObject.ticker;
-    //   this.resultsCount = chartObject.resultsCount;
-    //   this.closePrice = chartObject.results.map(price => price.c);
-    //   this.formattedTime = formattedTime;
-    // }
+  //   this.ticker = chartObject.ticker;
+  //   this.resultsCount = chartObject.resultsCount;
+  //   this.closePrice = chartObject.results.map(price => price.c);
+  //   this.formattedTime = formattedTime;
+  // }
 
-// [
-//   {
-//     "_id": "638923e743beb3ec18854364",
-//     "name": "Craig",
-//     "email": "craig@gmail.com",
-//     "portfolio": [
-//       {
-//         "ticker": "GOOG",
-//         "amountOwned": 20000,
-//         "boughtAt": 250
-//       }
-//     ],
-//     "__v": 0
-//   }
-// ]
+  // [
+  //   {
+  //     "_id": "638923e743beb3ec18854364",
+  //     "name": "Craig",
+  //     "email": "craig@gmail.com",
+  //     "portfolio": [
+  //       {
+  //         "ticker": "GOOG",
+  //         "amountOwned": 20000,
+  //         "boughtAt": 250
+  //       }
+  //     ],
+  //     "__v": 0
+  //   }
+  // ]
 
-//showBuyOrSellModal()
+  //showBuyOrSellModal()
 
-getUser = async () => {
-  try {
-    let results = await axios.get(`${SERVER}/user`);
-    this.setState({
-      userData: results.data[0],
-      userDataIsAvailable: true
-    });
-  } catch (error) {
-    console.log('we have an error: ', error.response.data);
+  getUser = async () => {
+    try {
+      let results = await axios.get(`${SERVER}/user`);
+      this.setState({
+        userData: results.data[0],
+        userDataIsAvailable: true
+      });
+    } catch (error) {
+      console.log('we have an error: ', error.response.data);
+    }
   }
-}
 
-componentDidMount() {
-  this.getUser();
-}
+  componentDidMount() {
+    this.getUser();
+  }
 
 
-  render(){
+  render() {
 
     //console.log(this.state.userData);
-    if(this.state.userData.portfolio){
+    if (this.state.userData.portfolio) {
       console.log(this.state.userData.portfolio);
     }
-    
 
-  return (
-<>
-    {/* //<Header with the Nav bar> */}
 
-    {/* //<div flexbox> 
+    return (
+      <>
+        <Header id="header" />
+        {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        {this.props.auth0.isAuthenticated ? <Profile /> : <h3>Please Log In</h3>}
+
+        {/* //<div flexbox> 
       //Tabs(for adding new asset) hard code the 15 to 20
       // Current balance(button) pass user's portfolio
     //<div flexbox> */}
-    {this.state.userDataIsAvailable &&
-       <AssetCards  
-       portfolio={this.state.userData.portfolio} 
-       // handleBuyOrSellModal = {this.showBuyOrSellModal}
-       />
-      
-    }
+        {this.state.userDataIsAvailable &&
+          <AssetCards
+            portfolio={this.state.userData.portfolio}
+          // handleBuyOrSellModal = {this.showBuyOrSellModal}
+          />
+
+        }
 
 
-    <p>Hello World</p>
-    </>
-  );
+        <p>Hello World</p>
+      </>
+    );
 
   }
 }
