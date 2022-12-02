06236f86af7d8bf88fa60fe516014a8cc20d946b
Todd Wolden <tcwolden@gmail.com>
2022-12-02 12:44:05 -0700

    auth0 working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from './Components/LoginButton.js';
+import LogoutButton from './Components/LogoutButton.js';
+import Profile from './Components/Profile.js';
+import Header from './Components/Header.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      isModalShown:false
+    this.state = {
+      isModalShown: false
     }
   }
-  render(){
-  return (
-    <>Hello World</>
-  );
+  render() {
+    return (
+      <>
+      <Header id="header" />
+        
+          {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
+          {this.props.auth0.isAuthenticated ? <Profile /> : <h3>Please Log In</h3>}
+      
+        Hello World
+
+      </>
+    );
   }
 }
 
-export default App;
+export default withAuth0(App);
