8a98046040a1399aaaf9a5f2c521cfda040dd440
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-12-05 17:19:19 -0800

    added css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from './LoginButton.js';
+import LogoutButton from './LogoutButton.js';
+import Profile from './Profile.js';
 import logo from './logo.jpg';
 
 class Header extends React.Component {
   render() {
     return (
       <>
-        {/* <div id="headerDiv">
-          <img width="170px" height="150px" src={logo} alt="My Virtual Trader Logo"></img>
-          <h1>My Virtual Trader</h1>
-        </div> */}
-
-
         <Navbar collapseOnSelect id="entireNavBar">
           <div className="container">
             <div className="row">
@@ -22,15 +20,22 @@ class Header extends React.Component {
               </div>
             </div>
           </div>
-          <NavItem id="navItemLanding"><Link to="/" className="nav-link" id="landingLink">Home</Link></NavItem>
+          <div id="title"><h1>My Virtual Portfolio</h1></div>
+          <div id="itemHolder">
+            <NavItem id="navItemLanding"><Link to="/" className="nav-link" id="landingLink">Home</Link></NavItem>
 
-          <NavItem id="navItemHome"><Link to="/App.js" className="nav-link" id="homeLink">Portfolio</Link></NavItem>
+            <NavItem id="navItemHome"><Link to="/App.js" className="nav-link" id="homeLink">Portfolio</Link></NavItem>
 
-          <NavItem id="navItemAbout"><Link to="/About.js" className="nav-link" id="aboutLink">About Us</Link></NavItem>
+            <NavItem id="navItemAbout"><Link to="/About.js" className="nav-link" id="aboutLink">About Us</Link></NavItem>
+          </div>
+          <div id="personal">
+            {this.props.auth0.isAuthenticated && <Profile />}
+            {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
+          </div>
         </Navbar>
       </>
     )
   }
 }
 
-export default Header;
+export default withAuth0(Header);
